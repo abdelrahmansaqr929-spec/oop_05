8476be1647197c0be1ef06733a7d5c72f9a00326
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-24 09:44:21 +0300

    answer01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            #region answer_01
+            // a) Both variables end up referring to the same object in memory,
+            //  No copy is made  you simply now have two names pointing to one object.
+            // b) no The assignment only copies the reference, not the object
+            //  Since both variables point to the same object, changing it through one variable will show up when you access it through the other.
+            //c)Copying a reference means two variables point to the same single object,
+            //so any change affects both. Copying the object itself means a brand new,
+            //independent object is created in memory, so changes to one no longer affect the other.
+            #endregion
         }
     }
 }
